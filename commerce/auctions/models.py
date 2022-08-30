c5f9f253e257903ab1ebac43c7c6bb68e43b4c1b
Enoch <iamenochchirima@gmail.com>
2022-08-30 17:30:29 +0200

    Create_listing can now add new listings into the database

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,3 +1,4 @@
+from sre_parse import CATEGORIES
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.db import models
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,8 +1,12 @@
+import re
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .forms import NewListingForm
+from datetime import datetime
 
 from .models import User
 
@@ -61,3 +65,23 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required(login_url='/accounts/login/')
+def create_listing(request):
+    submitted = False
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit= False)
+            instance.author = request.user
+            instance.save()
+            return HttpResponseRedirect("/create_listing?submitted=True")
+    
+    else:
+        form = NewListingForm()
+        if "submitted" in request.GET:
+            submitted = True
+    return render(request, "auctions/create_listing.html", {
+        "form": form,
+        "submitted": submitted,
+    })
